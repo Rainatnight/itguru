cc9b55ce5da377aaf17d1e0d458f0116e9b603c4
alex <alexnaf199@gmail.com>
2026-02-06 11:51:29 +0300

    feat: верхний блок

--- src/components/Catalog/TopBlock/TopBlock.tsx
+++ src/components/Catalog/TopBlock/TopBlock.tsx
@@ -1,17 +1,27 @@
+import { FaGlobe } from "react-icons/fa";
 import cls from "./TopBlock.module.scss";
-import { FiSearch, FiPlus, FiSettings, FiUser } from "react-icons/fi";
+import { FiBell, FiMail, FiSliders } from "react-icons/fi";
 
 export const TopBlock = () => {
   return (
     <div className={cls.block}>
       <h2 className={cls.title}>Товары</h2>
-      <input className={cls.input} type="text" placeholder="Поиск товаров..." />
-      <div className={cls.divider}></div>
-      <div className={cls.icons}>
-        <FiSearch size={24} />
-        <FiSettings size={24} />
-        <FiUser size={24} />
-        <FiPlus size={24} />
+      <div className={cls.inputWrap}>
+        <input
+          className={cls.input}
+          type="text"
+          placeholder="Поиск товаров..."
+        />
+      </div>
+
+      <div className={cls.right}>
+        <div className={cls.divider}></div>
+        <div className={cls.icons}>
+          <FaGlobe size={24} />
+          <FiBell size={24} />
+          <FiMail size={24} />
+          <FiSliders size={24} />
+        </div>
       </div>
     </div>
   );
